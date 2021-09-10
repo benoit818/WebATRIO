b90ccc705f14636e9296f3e9e8ca5a4682827417
benoit <benoi@DESKTOP-BCQDBVD>
2021-09-10 02:49:43 +0200

    pers changes

--- WebApplication1/Model/Person.cs
+++ WebApplication1/Model/Person.cs
@@ -8,6 +8,7 @@ namespace WebApplication1.Model
 {
     public class Person
     {
+        public Guid id { get; set; }
         public string Nom { get; set; }
         public string Prenom{get; set;}
         public DateTime Naissance { get; set; }
